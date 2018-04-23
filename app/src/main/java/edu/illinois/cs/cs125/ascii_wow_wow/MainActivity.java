34bd4827b042898194fdf16cf750373293b4a83f
sabrinali66 <sabrinali.66@gmail.com>
2018-04-23 17:07:20 -0500

    newActivity

--- app/src/main/java/edu/illinois/cs/cs125/ascii_wow_wow/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/ascii_wow_wow/MainActivity.java
@@ -1,13 +1,25 @@
 package edu.illinois.cs.cs125.ascii_wow_wow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button getActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button getActivity = (Button) findViewById(R.id.button);
+        getActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
     }
 }
