f426e575a436a3eefd5a8b8c8bd8452b77f36e8f
sabrinali66 <sabrinali.66@gmail.com>
2018-04-23 15:34:03 -0500

    Initial commit

---
+++ app/src/main/java/edu/illinois/cs/cs125/ascii_wow_wow/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs.cs125.ascii_wow_wow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
