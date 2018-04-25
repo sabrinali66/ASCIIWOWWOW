b6d0c679860a7db199073cd46595d3320ffac508
Manuel Flores <mflore67@illinois.edu>
2018-04-25 12:20:42 -0500

    Has functioning convert button and upload button opens gallery and lets you select image and put in imageview

--- app/src/main/java/edu/illinois/cs/cs125/ascii_wow_wow/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/ascii_wow_wow/MainActivity.java
@@ -15,6 +15,10 @@ import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
     private Button getActivity;
+    private ImageView imageView;
+    private Button button;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,44 +32,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Main2Activity.class));
             }
         });
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
     }
 
-    public class pickImage extends AppCompatActivity {
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
 
-        private ImageView imageView;
-        private Button button;
-        private static final int PICK_IMAGE = 100;
-        Uri imageUri;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            imageView = (ImageView) findViewById(R.id.imageView);
-            button = (Button) findViewById(R.id.button);
-
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openGallery();
-                }
-            });
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
         }
-
-        private void openGallery() {
-            Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-            startActivityForResult(gallery, PICK_IMAGE);
-        }
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-            if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
-                imageUri = data.getData();
-                imageView.setImageURI(imageUri);
-            }
-        }
-
     }
 }
